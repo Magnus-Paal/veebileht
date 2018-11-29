94309d581d7395a92ea430e8d6908c9d46db52bd
Sander Nemvalts <snemvalts@gmail.com>
2018-11-29 15:03:52 +0200

    alkod

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const hinnad = {
   'Caribba Negro 0.5L': [10.99, 6.65],
   'Sierra Silver tekiila 1L': [20, 11.5],
   'Beefeater London Dry 1L': [29.9,  17.99],
-  'Somersby Apple 0.5L': [1.29, 0.97],
+}
+
+const pildid = {
+  'Viru Valge 0.5L': '/alkod/viruvalge.png',
+  'Caribba Negro 0.5L': '/alkod/caribba.png',
+  'Sierra Silver tekiila 1L': '/alkod/sierra.png',
+  'Beefeater London Dry 1L': '/alkod/viruvalge.png',
 }
 const alecoq = [0.78, 0.6945]
 
@@ -16,21 +22,45 @@ function kalkuleeri(){
 
   let palju = kütusele;
   const alko = [];
+  const domel = [];
   for (const key in hinnad) {
     const delta = hinnad[key][0] - hinnad[key][1];
-    const arv = Math.min(3, Math.floor((palju/1.25)/delta));
+    const arv = Math.min(5, Math.floor((palju/1.25)/delta));
     if (arv === 0) {
       break;
     } else {
       palju -=  delta * arv;
       alko.push(`${arv}x ${key}`);
+
+      for(let i = 0; i < arv; i++) {
+        let el = document.createElement('img');
+        el.src = pildid[key];
+        el.classList.add('jook')
+        domel.push(el);
+      }
     }
   }
 
+  let alecoqy = 0;
+
   if (palju > alecoq[0] - alecoq[1]) {
     alko.push(`${Math.floor(palju / (alecoq[0] - alecoq[1]))}x A.Le.Coq Premium 0.5L`);
+    alecoqy = Math.floor((palju / (alecoq[0] - alecoq[1]))/24);
     palju = 0;
   }
-  console.log(alko, palju, kütusele);
+
+  console.log(alecoqy);
+  for(let i = 0; i < alecoqy; i++) {
+    let el = document.createElement('img');
+    el.src = '/alkod/alecoq.png';
+    el.classList.add('jook')
+    domel.push(el);
+  }
+
   document.querySelector('.tulemus p').innerHTML = 'Selleks, et Lätti minemine väärt oleks, peaksite Valkast ostma: '+ alko.join(', ');
+
+  document.querySelector('.joogid').innerHTML = '';
+  domel.forEach((el) => {
+    document.querySelector('.joogid').appendChild(el);
+  });
 }
